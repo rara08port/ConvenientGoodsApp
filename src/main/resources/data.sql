9d19400472a6f5c39bc8db015f3feaa64eba4097
rara08port <k.kblsh4058@gmail.com>
2021-08-17 20:21:12 +0900

    add 初期値データ作成SQL
    テーブル作成、データ挿入SQLを作成しました

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,32 @@
 INSERT INTO employee (id, name, age)
 VALUES('1', 'Tom', 30);
+
+
+
+INSERT INTO users (id,username,password,msg,user_img,delete_flg,login_time,created_date)
+VALUES(1, 'dog','dogdog','test msg', 'uploads/puppy.jpg', 0, '2019-05-31 20:45:40', '2019-05-31 11:45:40');
+
+
+
+INSERT INTO post (id, contents, post_img, user_id, category_id, delete_flg, created_date) 
+VALUES(1, 'tanaka', 'uploads/puppy.jpg',1,1,0, '2019-06-17 02:21:20');
+
+
+INSERT INTO comment (id, post_id, user_id,comment, delete_flg, created_date) 
+VALUES(1,1,1,'comment test',0, '2019-06-17 02:21:20');
+
+
+
+INSERT INTO category (id, category_name) 
+VALUES(1,'money');
+
+
+
+INSERT INTO good (id, post_id, user_id,delete_flg, created_date) 
+VALUES(1,1,1,0, '2019-06-17 02:21:20');
+
+
+
+INSERT INTO favarite (id, post_id, user_id,delete_flg, created_date) 
+VALUES(1,1,1,0, '2019-06-17 02:21:20');
+
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,3 +5,58 @@ CREATE TABLE IF NOT EXISTS employee (
 );
 
 
+CREATE TABLE  users  (
+   id  int(11) NOT NULL,
+   username  varchar(255) NOT NULL,
+   password  varchar(255) NOT NULL,
+   msg  text NOT NULL,
+   user_img  varchar(255) NOT NULL,
+   delete_flg  tinyint(1) NOT NULL DEFAULT '0',
+   login_time  datetime NOT NULL,
+   created_date  timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE  post  (
+   id  int(11) NOT NULL,
+   contents  text NOT NULL,
+   post_img  varchar(255) NOT NULL,
+   user_id  int(11) NOT NULL,
+  category_id int(11) NOT NULL,
+   delete_flg  tinyint(1) NOT NULL DEFAULT '0',
+   created_date  timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE  comment  (
+   id  int(11) NOT NULL,
+   post_id  int(11) NOT NULL,
+   user_id  int(11) NOT NULL,
+   comment  text NOT NULL,
+   delete_flg  tinyint(1) NOT NULL DEFAULT '0',
+   created_date  timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE  category  (
+   id  int(11) NOT NULL,
+   category_name  varchar(255) NOT NULL
+);
+
+
+CREATE TABLE  good  (
+   id  int(11) NOT NULL,
+   post_id  int(11) NOT NULL,
+   user_id  int(11) NOT NULL,
+   delete_flg  tinyint(1) NOT NULL DEFAULT '0',
+   created_date  timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE  favarite  (
+   id  int(11) NOT NULL,
+   post_id  int(11) NOT NULL,
+   user_id  int(11) NOT NULL,
+   delete_flg  tinyint(1) NOT NULL DEFAULT '0',
+   created_date  timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+

e3d9f9c7f1600c5173e83115962447e33a579960
rara08port <k.kblsh4058@gmail.com>
2021-08-18 17:51:16 +0900

    add Sprign Security Gradle 追加
    SpringSecurityを使うためにgradleに追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,6 +11,9 @@ VALUES(1, 'dog','dogdog','test msg', 'uploads/puppy.jpg', 0, '2019-05-31 20:45:4
 INSERT INTO post (id, contents, post_img, user_id, category_id, delete_flg, created_date) 
 VALUES(1, 'tanaka', 'uploads/puppy.jpg',1,1,0, '2019-06-17 02:21:20');
 
+INSERT INTO post (id, contents, post_img, user_id, category_id, delete_flg, created_date) 
+VALUES(1, 'tanaka', 'uploads/puppy.jpg',1,2,0, '2019-06-17 02:21:20');
+
 
 INSERT INTO comment (id, post_id, user_id,comment, delete_flg, created_date) 
 VALUES(1,1,1,'comment test',0, '2019-06-17 02:21:20');
@@ -19,7 +22,8 @@ VALUES(1,1,1,'comment test',0, '2019-06-17 02:21:20');
 
 INSERT INTO category (id, category_name) 
 VALUES(1,'money');
-
+INSERT INTO category (id, category_name) 
+VALUES(2,'life');
 
 
 INSERT INTO good (id, post_id, user_id,delete_flg, created_date) 
